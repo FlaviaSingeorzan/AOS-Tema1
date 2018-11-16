68178a7708bfa2c0f0d702567f8c87ca1639c7e1
Flavia-Ana Singeorzan <singeorzan_flavia@yahoo.com>
2018-11-16 23:56:21 +0200

    Tema2-NotaSapte

---
+++ Tema2-NotaSapte/src/main/java/com/unitbv/lab/tema2NotaSapte/Tema2NotaSapteApplication.java
@@ -0,0 +1,12 @@
+package com.unitbv.lab.tema2NotaSapte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tema2NotaSapteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tema2NotaSapteApplication.class, args);
+	}
+}
